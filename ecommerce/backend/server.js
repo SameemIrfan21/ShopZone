fb7f1a6e6f95dd5ed57c7b72bfca82765748b87f
SameemIrfan21 <sameemirfan53@gmail.com>
2026-03-20 22:48:30 +0530

    fix: production server fix, vercel.json update, add .gitignore

--- ecommerce/backend/server.js
+++ ecommerce/backend/server.js
@@ -52,8 +52,6 @@ async function seedAdmin() {
 }
 
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-}
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 module.exports = app;
